fbfd7c5b466dbbad64e7e0f2026117cfa42fa534
PelekhIgor <igorpelekh0112@gmail.com>
2022-05-16 12:32:30 +0300

    first comit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const burger = document.querySelector('.header_burger')
+const menu = document.querySelector('.header_menu')
+
+burger.addEventListener('click', onClick)
+
+    function onClick(){
+     menu.classList.toggle('_active')
+
+}
+
+
+
